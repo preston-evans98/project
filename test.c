2a9a62ceaaf08c84362ae47e578c2821190aab13
Preston Evans <pbevans1@crimson.ua.edu>
2019-11-16 12:54:00 -0600

    formatting test.c

--- test.c
+++ test.c
@@ -3,48 +3,55 @@
 #include "matrix.h"
 
 // This file assumes that compute is already running in another terminal
-bool runTest(char* testInputOne, char* testInputTwo, char* resultFileName) {
+bool runTest(char *testInputOne, char *testInputTwo, char *resultFileName)
+{
     char command[500];
     sprintf(command, "./package %s.dat %s.dat result.dat 0", testInputOne, testInputTwo);
     system(command);
-    FILE* resultFp = fopen("result.dat", "r");
-    if (!resultFp) {
+    FILE *resultFp = fopen("result.dat", "r");
+    if (!resultFp)
+    {
         fprintf(stderr, "Could not open result file.");
     }
-    FILE* expectedFp = fopen(resultFileName, "r");
-    if (!expectedFp) {
+    FILE *expectedFp = fopen(resultFileName, "r");
+    if (!expectedFp)
+    {
         fprintf(stderr, "Could not open expected file.");
     }
-    Matrix* actual = readMatrixFromFile(resultFp);
-    Matrix* expected = readMatrixFromFile(expectedFp);
-    if (!matrixEquals(actual, expected)){
+    Matrix *actual = readMatrixFromFile(resultFp);
+    Matrix *expected = readMatrixFromFile(expectedFp);
+    if (!matrixEquals(actual, expected))
+    {
         fprintf(stderr, "%s * %s != %s\n", testInputOne, testInputTwo, resultFileName);
         return false;
     }
     return true;
 }
 
-
-
-int main() {
+int main()
+{
     bool success;
     success = runTest("matrix1", "matrix2", "expected3x5.dat");
-    if (!success) {
+    if (!success)
+    {
         printf("FAILURE!\n");
         exit(-1);
     }
     success = runTest("large", "l3", "expected50x5.dat");
-    if (!success) {
+    if (!success)
+    {
         printf("FAILURE!\n");
         exit(-1);
     }
     success = runTest("large", "large", "expected50x50.dat");
-    if (!success) {
+    if (!success)
+    {
         printf("FAILURE!\n");
         exit(-1);
     }
     success = runTest("small", "small", "expected1x1.dat");
-    if (!success) {
+    if (!success)
+    {
         printf("FAILURE!\n");
         exit(-1);
     }
